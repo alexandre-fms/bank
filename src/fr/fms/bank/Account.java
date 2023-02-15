73e92947899e4ef5f6236aa54287c2a46128d24b
Dylan DEA <dylan.de-albuquerque@fms-ea.com>
2023-02-15 14:20:43 +0100

    création classe abstraite Account

--- src/fr/fms/bank/Account.java
+++ src/fr/fms/bank/Account.java
@@ -1,5 +1,48 @@
 package fr.fms.bank;
 
-public class Account {
-
+public abstract class Account {
+	private int currentAmount;
+	private String ID;
+	private String label;
+	
+	public Account(String ID, String label, int currentAmount) {
+		setCurrentAmount(currentAmount);
+		setID(ID);
+		setLabel(label);	
+	}
+	
+	public int getCurrentAmount() {
+		return currentAmount;
+	}
+	
+	public String getID() {
+		return ID;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public void setCurrentAmount(int currentAmount) {
+		this.currentAmount = currentAmount;
+	}
+	
+	public void setID(String ID) {
+		this.ID = ID;
+	}
+	
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
+	public int displayAvailableAmount() {
+		return this.currentAmount;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [ID=" + ID + ", label=" + label + ", currentAmount=" + currentAmount + "]";
+	}
+	
+	
 }
