ecee0867ba42ecc4f8213ff1d95c0d8b5673f817
Nicolas GUYAU <GuyauN@ad.fms-ea.com>
2023-02-15 15:26:52 +0100

    lemessage

--- src/fr/fms/bank/Customer.java
+++ src/fr/fms/bank/Customer.java
@@ -1,5 +1,46 @@
 package fr.fms.bank;
 
 public class Customer extends User{
+	private String savingAccountId;
+	private String currentAccountId;
+	
+	public Customer (String savingAccountId,String currentAccountId,String ID,String email,String lastName,String firstName,String adress){
+		super(ID,email,lastName,firstName,adress);
+		setSavingAccountId(savingAccountId);
+		setCurrentAccountId(currentAccountId);
+	
+	}
+	
+	public String getSavingAccountId() {
+		return savingAccountId;
+	}
 
-}
+	public void setSavingAccountId(String savingAccountId) {
+		this.savingAccountId = savingAccountId;
+	}
+
+	public String getCurrentAccountId() {
+		return currentAccountId;
+	}
+
+	public void setCurrentAccountId(String currentAccountId) {
+		this.currentAccountId = currentAccountId;
+	}
+
+	
+	public boolean virement(String currentAccountId,String currentAccountId2,int amount) {
+		return true;
+	}
+	public int retire(String currentAccountId,int amount) {
+		return amount;
+	}
+	public int versement(String currentAccountId,int amount) {
+		return amount;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [savingAccountId=" + savingAccountId + ", currentAccountId=" + currentAccountId + super.getLastName()+super.getFirstName()+"]";
+	}
+	
+	}
