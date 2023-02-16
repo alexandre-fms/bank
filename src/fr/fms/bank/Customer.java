5065ebee05d7b81d7aee0771df8052abf1a39626
Nicolas GUYAU <GuyauN@ad.fms-ea.com>
2023-02-16 11:23:21 +0100

    grouink

--- src/fr/fms/bank/Customer.java
+++ src/fr/fms/bank/Customer.java
@@ -1,13 +1,34 @@
 package fr.fms.bank;
 
-public class Customer extends User{
+public class Customer {
 	private String savingAccountId;
 	private String currentAccountId;
+	private String Id;
+	private String lastName;
+	private String firstName;
+	private String adress;
+	private String email;
 	
-	public Customer (String savingAccountId,String currentAccountId,String ID,String email,String lastName,String firstName,String adress){
-		super(ID,email,lastName,firstName,adress);
+	/**
+	 * Creation du constructeur pour la class customer avec 
+	 * lien a la classe mere User
+	 * @param savingAccountId
+	 * @param currentAccountId
+	 * @param Id
+	 * @param email
+	 * @param lastName
+	 * @param firstName
+	 * @param adress
+	 */
+	public Customer (String savingAccountId,String currentAccountId,String Id,String email,String lastName,String firstName,String adress){
+		//super(ID,email,lastName,firstName,adress);
 		setSavingAccountId(savingAccountId);
 		setCurrentAccountId(currentAccountId);
+		setId(Id);
+		setEmail(email);
+		setLastName(lastName);
+		setFirstName(firstName);
+		setAdress(adress);
 	
 	}
 	
@@ -27,7 +48,44 @@ public class Customer extends User{
 		this.currentAccountId = currentAccountId;
 	}
 
-	
+	public String getId() {
+		return Id;		
+	}
+	public void setId(String Id) {
+		this.Id = Id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public boolean virement(String currentAccountId,String currentAccountId2,int amount) {
 		return true;
 	}
@@ -40,7 +98,12 @@ public class Customer extends User{
 
 	@Override
 	public String toString() {
-		return "Customer [savingAccountId=" + savingAccountId + ", currentAccountId=" + currentAccountId + super.getLastName()+super.getFirstName()+"]";
+		return "Customer [savingAccountId=" + savingAccountId + ", currentAccountId=" + currentAccountId + ", Id=" + Id
+				+ ", lastName=" + lastName + ", firstName=" + firstName + ", adress=" + adress + ", email=" + email
+				+ "]";
 	}
+
+	
+	
 	
 	}
