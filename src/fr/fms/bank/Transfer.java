6ed926fcb5e4e131297eb9a38eeb1fe4026d3a17
Aurore Chevin <chevinaurore@gmail.com>
2023-02-16 11:43:41 +0100

    Création classe Operation et ses classes filles

---
+++ src/fr/fms/bank/Transfer.java
@@ -0,0 +1,14 @@
+package fr.fms.bank;
+
+import java.util.Date;
+
+public class Transfer extends Operation{
+	public Transfer(int id, double amount, String date) {
+		super(id, amount, date);
+	}
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+	
+}
---
+++ src/fr/fms/bank/Withdraw.java
@@ -0,0 +1,16 @@
+package fr.fms.bank;
+
+
+
+public class Withdraw extends Operation{
+	public Withdraw(int id, double amount, String date) {
+		super(id,amount, date);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+	
+	
+}
