29037077248fb313d6474e57557df689072a5aba
Nicolas GUYAU <GuyauN@ad.fms-ea.com>
2023-02-17 10:53:39 +0100

    le taff

--- src/fr/fms/bank/SavingAccount.java
+++ src/fr/fms/bank/SavingAccount.java
@@ -1,42 +1,65 @@
 package fr.fms.bank;
-
+/**
+ * 
+ * @author GuyauN,ChevinA,ArenasA,DealbuquerqueD;
+ *Permet de creer un compte épargne.
+ */
 public class SavingAccount extends Account {
 	double interestRate;
-
-	public SavingAccount(double interestRate, Customer customer) {
-		super(customer);
+/**
+ * //permet de contruire le compte epargne d'un client;
+ * @param interestRate double taux d'interet
+ * @param customer client de la banque;
+ */
+	public SavingAccount(double interestRate, Customer customer) {  
+		super(customer);												
 		setInterestRate(interestRate);
 	}
 
-
-	private void setInterestRate(double interestRate2) {
+/**
+ * 
+ * @param interestRate2  double permet de modifier le taux d'interet d'un compte;
+ */
+	private void setInterestRate(double interestRate2) {			
 		this.interestRate = interestRate2;
 
 	}
 
-
+/**
+ * 
+ * @param amount entier permet de modifier le solde  d'un compte;
+ */
 	public void setCurrentAmount(int amount) {
-		super.setCurrentAmount(amount);		
+		super.setCurrentAmount(amount);								
 	}
 
+	/**
+	 * permet de calculer le gain d'argent d'un compte epargne en fonction du taux.
+	 */
 	public void applyInterest() {
-		super.setCurrentAmount(super.getCurrentAmount() + (super.getCurrentAmount() * interestRate));
+		super.setCurrentAmount(super.getCurrentAmount() + (super.getCurrentAmount() * interestRate)); 
 	}
-
-	public void addMoney(double amount) {
+	/**
+	 *  amount entier permet d'ajouter de l'argent a un compte epargne;
+	 */
+	public void addMoney(double amount) {			
 
 	}
-
+	/**
+	 * permet de retirer de l'argent a un compte en verifiant sa capacité de retrait;
+	 */
 	public void removeMoney(double amount) {
-		if ((super.getCurrentAmount() - amount) > 0) {
+		if ((super.getCurrentAmount() - amount) > 0) {					
 			super.setCurrentAmount(super.getCurrentAmount() - amount);
 		} 
 		else System.out.println("vous avez dépassé vos capacités de retrait !");
 	}
 
-
+	/**
+	 * affiche les infos d'un compte epargne en lien avec la classe account;
+	 */
 	@Override
-	public String toString() {
+	public String toString() {														
 		return "Saving Account:" + super.toString() + ", rate="+interestRate;
 	}
 }
--- src/fr/fms/bank/Transfer.java
+++ src/fr/fms/bank/Transfer.java
@@ -1,10 +1,17 @@
 package fr.fms.bank;
 
-
+/**
+ * 
+ * @author GuyauN,ChevinA,ArenasA,DealbuquerqueD;
+ *donne le montant  du transfer entre un compte et un autre.
+ */
 public class Transfer extends Operation{
 	public Transfer(double amount) {
-		super(amount);
+		super(amount);						//
 	}
+	/**
+	 * Affiche les details d'un transfer.
+	 */
 	@Override
 	public String toString() {
 		return "Détail du transfert " + super.toString();
